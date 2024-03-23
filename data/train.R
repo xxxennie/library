f2339e3d545a27ccf42c4c9993933d5f207ce2d5
xxxennie <133770771+xxxennie@users.noreply.github.com>
2024-03-23 01:35:49 -0700

    Add files via upload

---
+++ data/train.R
@@ -0,0 +1,7 @@
+#' Description of the train dataset.
+#'
+#' This dataset contains information about training data.
+#' 
+#' @format A data frame with ...
+#' @source Where the data comes from.
+"train"
